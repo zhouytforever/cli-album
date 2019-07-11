0d45b1c7b1f0c1a11a3bacc4429e85c8e225ccaa
Joyt <zhouytforever@gmail.com>
2019-07-11 07:55:04 -0400

    fix bug of last page

--- src/components/Album/_.js
+++ src/components/Album/_.js
@@ -12,6 +12,8 @@ export default {
     }
   },
   computed: {
+    showPre: ({currentIndex, page}) => currentIndex>0 && page.length>0,
+    showSuf: ({currentIndex, page}) => currentIndex < page.length -1,
     current: ({ currentIndex, page }) => page[currentIndex] ||  { title: 'loading 。。。'},
     pre: ({ currentIndex, page }) => {
       if(page.length === 0) {
@@ -32,12 +34,11 @@ export default {
       const { currentIndex, page, pageNum } = this
       if(currentIndex<page.length -2){
         this.currentIndex ++
-      }
-      else {
-        const {data} = await a.findOnePage(pageNum + 1)
-        if(!data || data.length === 0) return
+      } else {
         this.currentIndex ++
         this.pageNum ++
+        const {data} = await a.findOnePage(pageNum + 1)
+        if(!data || data.length === 0) return
         this.page = page.concat(data)
       }
     },
--- src/components/Album/index.vue
+++ src/components/Album/index.vue
@@ -4,11 +4,11 @@
       <h3>{{current.title}}</h3>
     </header>
     <PicGroup :pics="current.pics" ref="picGroup"></PicGroup>
-    <a class="pre" @click="before">
+    <a class="pre" @click="before"  v-show="showPre">
       <span class="arrow-left"></span>
       <span class="title">{{pre.title}}</span>
     </a>
-    <a class="suf" @click="next">
+    <a class="suf" @click="next" v-show="showSuf">
       <span class="title">{{suf.title}}</span>
       <span class="arrow-right"></span>
     </a>
